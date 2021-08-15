90a8435a431f73508537bcd342c77d330b6d5cde
darkness-falls <88970024+darkness-falls@users.noreply.github.com>
2021-08-15 21:15:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+from colorama import Fore, Back, Style
+
+print("Телеграм спаммер")
+
+print(Fore.LIGHTCYAN_EX + """
+    ____                      __     ____        _     __              
+   / __ \___  ________  _____/ /_   / __ \____ _(_)___/ /__  __________
+  / / / / _ \/ ___/ _ \/ ___/ __/  / /_/ / __ `/ / __  / _ \/ ___/ ___/
+ / /_/ /  __(__  )  __/ /  / /_   / _, _/ /_/ / / /_/ /  __/ /  (__  ) 
+/_____/\___/____/\___/_/   \__/  /_/ |_|\__,_/_/\__,_/\___/_/  /____/  
+                                                                                                                                            
+""")
+
+print("""
+88. Присоидениться к приватному чату
+99. Присоидениться к публичному каналу
+
+1. Спам заданным сообщениями/картинками
+2. Спам хентаем (медленно но разными гифками)
+3. Спам хентаем (быстро но одной гифкой)
+""")
+
+while True:
+	user_input = int(input(">>> "))
+
+	if user_input == 2:
+		os.system("python3 core/slow_hentai_spammer.py")
+	elif user_input == 1:
+		os.system("python3 core/sms_spammer.py")
+	elif user_input == 3:
+		os.system("python3 core/fast_hetai.py")
+	elif user_input == 88:
+		os.system("python3 core/join_chat.py")
+	elif user_input == 99:
+		os.system("python3 core/join_chat1.py")
+	else:
+		print("Ошибка")
